a36e02a3830e506d3bb675549859fd5ac9685e18
Sakshi-Kumari26 <SAKSHI-KUMARI.A.SAKSHI-KUMARI@CAPGEMINI.COM>
2026-06-03 11:55:49 +0530

    Revert "Update TodoDTO.java"
    This reverts commit fea34d4d33a6cc3e77ce39991d2f6a3261d15b80.

--- todo-service/src/main/java/com/zs/dto/TodoDTO.java
+++ todo-service/src/main/java/com/zs/dto/TodoDTO.java
@@ -3,7 +3,7 @@ package com.zs.dto;
 import jakarta.validation.constraints.Pattern;
 
 public class TodoDTO {
-	@Pattern(regexp = "[A-Z][a-z ]{5,}", message = "Enter valid task details")//regex exp.
+	@Pattern(regexp = "[A-Z][a-z ]{5,}", message = "Enter valid task details")
 	private String task;
 
 	public String getTask() {
